3bf7a3b1dece9aa4e27196ab99d98c091e2b698c
Khlidbg78dem <khalidjj78200@icloud.com>
2024-05-26 12:16:17 +0200

    logo

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,6 @@
     <div class="">
         <h1>AFAPS</h1>
     </div>
-
     <div class="card" style="width: 18rem;">
         <img src="..." class="card-img-top" alt="...">
         <div class="card-body">
@@ -11,6 +10,7 @@
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
+        <div></div>
       </div>
       <div class="card mb-3">
         <img src="..." class="card-img-top" alt="...">
@@ -31,7 +31,7 @@
     </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 
 </script>
 
